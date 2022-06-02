e60d64b8a771716ef7e75ea987ba1e9170539008
HaleemaEssa <70490977+HaleemaEssa@users.noreply.github.com>
2022-06-02 21:33:28 +0300

    Update p.py

--- p.py
+++ p.py
@@ -41,8 +41,9 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel1, GPIO.IN)
 GPIO.setup(channel12, GPIO.IN)
 #GPIO.setup(channel14, GPIO.IN)
+channel25 =25
 GPIO.setup(channel25, GPIO.OUT)
-channel25 =25 #for relay
+#channel25 =25 #for relay
 credentials = pika.PlainCredentials('haleema', '4chyst')
 parameters = pika.ConnectionParameters('192.168.0.126',
                                    5672,
@@ -66,25 +67,23 @@ def callback1(channel1):
     sound=0
 GPIO.add_event_detect(channel1, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel1, callback1)  # assign function to GPIO PIN, Run function on change
-
 def callback2(channel12):
     if GPIO.input(channel12):
         GPIO.output(25,GPIO.LOW)
         time.sleep(2)    
         return('0')
-        
+
     else:
         print("led on")
-        blynk.log_event('flame_event', 'Hello') #flame_event
+        #blynk.log_event('flame_event', 'Hello') #flame_event
         GPIO.output(25,GPIO.HIGH)
+        blynk.log_event('flame_event', 'Hello') #flame_event
         time.sleep(2)
         return('1')
     return flame    #
     flame=0
 GPIO.add_event_detect(channel12, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel12, callback2)  # assign function to GPIO PIN, Run function on change
-
-
 def callback4():
 
     dht11 = adafruit_dht.DHT11(SENSOR_PIN, use_pulseio=False)
@@ -126,27 +125,41 @@ def callback4():
 ###@# infinite loop
 #while True:
  #       time.sleep(3)
-
-def checkdht():
+ def checkdht():
     for i in range(10):
         try:
             h, t =callback4()
             sound=callback1(channel1)
             flame=callback2(channel12)
-            message=str(sound)+":"+str(flame)+":"+str(h)+":"+str(t)
-            channel.basic_publish(exchange='logs', routing_key='', body= message)
-            print ("sent %r" %message) 
-           ########
+######
             blynk.run()
             blynk.virtual_write(T_PIN, t)
             blynk.virtual_write(H_PIN, h)
             blynk.virtual_write(F_PIN, flame*255)
             blynk.virtual_write(S_PIN, sound*100)
-            if temperature > 20:
-                blynk.log_event('temp_event', 'Hello')
-            if humidity > 25:
-                blynk.log_event('humidity_event', 'Hello')  
-            print('Temperature: ',temperature,'\tHumidity: ',humidity,'\t\tFlame: ',fla$
+            if t > 20:
+                blynk.log_event('temp_event', 'Humidity is High')
+            if h > 27:
+                blynk.log_event('humidity_event', 'Temperature is High')
+        #    if flame == 1:
+         #       blynk.log_event('flame_event', 'Flame Notification')
+          #  if sound == 1:
+           #     blynk.log_event('sound_event', 'Sound Notification')  
+
+            message=str(sound)+":"+str(flame)+":"+str(h)+":"+str(t)
+            channel.basic_publish(exchange='logs', routing_key='', body= message)
+            print ("sent %r" %message) 
+########
+            #blynk.run()
+            #blynk.virtual_write(T_PIN, t)
+            #blynk.virtual_write(H_PIN, h)
+            #blynk.virtual_write(F_PIN, flame*255)
+            #blynk.virtual_write(S_PIN, sound*100)
+            #if t > 20:
+            #    blynk.log_event('temp_event', 'Hello')
+            #if h > 27:
+            #    blynk.log_event('humidity_event', 'Hello')  
+            #print('Temperature: ',t,'\tHumidity: ',h,'\t\tFlame: ',flame,'\tSound: ',sound)
 
         except RuntimeError:
             pass
@@ -156,3 +169,4 @@ while True:
     checkdht()
 
 connection.close()
+
